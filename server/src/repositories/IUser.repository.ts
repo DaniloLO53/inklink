06a6089aece1c8a54806171cffdd5a3ea4497699
Danilo Leão <olin_ad@hotmail.com>
2023-03-26 18:46:04 -0300

    feat: createUser usecase

--- server/src/repositories/IUser.repository.ts
+++ server/src/repositories/IUser.repository.ts
@@ -2,5 +2,5 @@ import { User } from "../entities/user/User";
 
 export interface IUsersRepository {
   findByEmail(email: string): Promise<User>;
-  save(user: User): Promisse<void>;
+  save(user: User): Promise<void>;
 };
--- server/src/useCases/CreateUser/CreateUser.useCase.ts
+++ server/src/useCases/CreateUser/CreateUser.useCase.ts
@@ -1,5 +1,19 @@
-export class CreateUserUseCase {
-  async create() {
+import { User } from "../../entities/user/User";
+import { IUsersRepository } from "../../repositories/IUser.repository";
+import { ICreateUserRequestDTO } from "./CreateUser.tdo";
 
+export class CreateUserUseCase {
+  constructor(private userRepository: IUsersRepository) {}
+
+  async create(data: ICreateUserRequestDTO) {
+    const userAlreadyExists = await this.userRepository.findByEmail(data.email);
+
+    if (userAlreadyExists) {
+      throw new Error('User already exists');
+    }
+
+    const user = new User(data);
+
+    this.userRepository.save();
   }
 };
